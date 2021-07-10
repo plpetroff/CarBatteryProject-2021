2c48c07afda45337d0ea10ca5402398dafca916a
Pl Petroff <plpetroff@gmail.com>
2021-07-10 12:23:31 +0300

    Seed Categories

---
+++ CarBatteries/Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace CarBatteries.Infrastructure
+{
+    using CarBatteries.Data;
+    using Microsoft.AspNetCore.Builder;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.EntityFrameworkCore;
+    using CarBatteries.Data.Models;
+
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder PrepareDatabase(this IApplicationBuilder app)
+        {
+            
+            using var scopedServices = app.ApplicationServices.CreateScope();
+
+            var data = scopedServices.ServiceProvider.GetService<CarBatteriesDbContext>();
+
+            data.Database.Migrate();
+
+            SeedCategories(data);
+
+            return app;
+        }
+
+        private static void SeedCategories(CarBatteriesDbContext data)
+        {
+            if (data.Categories.Any())
+            {
+                return;
+            }
+
+            data.Categories.AddRange(new[]
+            {
+                new Category{ CategoryName = "SMF Power"},
+                new Category{ CategoryName = "Truck"},
+                new Category{ CategoryName = "Truck SMF"},
+                new Category{ CategoryName = "Marine"},
+                new Category{ CategoryName = "EFB"},
+                new Category{ CategoryName = "AGM"},
+
+            });
+        }
+    }
+}
--- CarBatteries/Startup.cs
+++ CarBatteries/Startup.cs
@@ -3,6 +3,7 @@
 namespace CarBatteries
 {
     using CarBatteries.Data;
+    using CarBatteries.Infrastructure;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.HttpsPolicy;
@@ -50,6 +51,8 @@ namespace CarBatteries
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.PrepareDatabase();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
